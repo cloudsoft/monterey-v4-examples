57e64a2f1a7259a9bafb8394ffb419d9656e6891
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2011-09-29 11:23:16 +0100

    Updated sample applications

--- src/main/java/example/pingpong/PingActor.java
+++ src/main/java/example/pingpong/PingActor.java
@@ -2,26 +2,38 @@ package example.pingpong;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.google.common.base.Preconditions;
+
 import monterey.actor.Actor;
 import monterey.actor.ActorContext;
 import monterey.actor.ActorRef;
 import monterey.actor.MessageContext;
+import monterey.logging.Logger;
+import monterey.logging.LoggerFactory;
 
 public class PingActor implements Actor {
+    private static final Logger LOG = new LoggerFactory().getLogger("Actor");
+
+    public static final String MESSAGE_KEY = "message";
+    public static final String MESSAGE_FORMAT = "%s: %d";
+
     private ActorContext context;
-    private ActorRef pong;
     private AtomicLong count = new AtomicLong(0L);
-    private String message = "PING %d";
+    private String message;
+    private ActorRef pong;
     
     public void init(ActorContext context) {
+        Preconditions.checkArgument(context.getConfigurationParams().containsKey(MESSAGE_KEY), "Message configuration not set");
         this.context = context;
-        
+        this.message = context.getConfigurationParams().get(MESSAGE_KEY);
+        LOG.info("Ping initialised with '%s'", message);
+
         pong = context.lookupActor("pong");
         context.sendTo(pong, "INIT");
     }
 
     public void onMessage(Object payload, MessageContext messageContext) {
-        System.err.printf("Ping got '%s'", (String) payload);
-        context.sendTo(pong, String.format(message, count.incrementAndGet()));
+        LOG.info("Ping got '%s'", (String) payload);
+        context.sendTo(messageContext.getSource(), String.format(MESSAGE_FORMAT, message, count.incrementAndGet()));
     }
 }
--- src/main/java/example/pingpong/PongActor.java
+++ src/main/java/example/pingpong/PongActor.java
@@ -2,21 +2,33 @@ package example.pingpong;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.google.common.base.Preconditions;
+
 import monterey.actor.Actor;
 import monterey.actor.ActorContext;
 import monterey.actor.MessageContext;
+import monterey.logging.Logger;
+import monterey.logging.LoggerFactory;
 
 public class PongActor implements Actor {
+    private static final Logger LOG = new LoggerFactory().getLogger("Actor");
+
+    public static final String MESSAGE_KEY = "message";
+    public static final String MESSAGE_FORMAT = "%s: %d";
+
     private ActorContext context;
     private AtomicLong count = new AtomicLong(0L);
-    private String message = "PONG %d";
+    private String message;
     
     public void init(ActorContext context) {
+        Preconditions.checkArgument(context.getConfigurationParams().containsKey(MESSAGE_KEY), "Message configuration not set");
         this.context = context;
+        this.message = context.getConfigurationParams().get(MESSAGE_KEY);
+        LOG.info("Pong initialised with '%s'", message);
     }
 
     public void onMessage(Object payload, MessageContext messageContext) {
-        System.err.printf("Pong got '%s'", (String) payload);
-        context.sendTo(messageContext.getSource(), String.format(message, count.incrementAndGet()));
+        LOG.info("Pong got '%s'", (String) payload);
+        context.sendTo(messageContext.getSource(), String.format(MESSAGE_FORMAT, message, count.incrementAndGet()));
     }
 }
