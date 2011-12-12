014489a1d9d8d6b29d11aba36b27c2db9e3e74fe
Aled Sage <aled@Aled-Sages-MacBook-Pro.local>
2011-12-12 16:21:27 +0000

    EchoTest: use VenueTestHarness.addMessageListener to test for response

--- simple-actors/src/test/java/monterey/example/echo/EchoTest.java
+++ simple-actors/src/test/java/monterey/example/echo/EchoTest.java
@@ -1,16 +1,28 @@
 package monterey.example.echo;
 
+import static org.testng.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import monterey.actor.ActorRef;
 import monterey.actor.ActorSpec;
+import monterey.actor.MessageContext;
+import monterey.actor.MessageListener;
 import monterey.venue.testharness.VenueTestHarness;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 public class EchoTest {
 
+    private static final long TIMEOUT = 10*1000;
+            
     VenueTestHarness harness;
 
     @BeforeMethod
@@ -29,11 +41,26 @@ public class EchoTest {
         assertTrue(harness.getActorInstance(actorRef) instanceof EchoActor);
     }
 
-    @Test//(enabled=false)
-    public void testEchoedResponse() {
+    @Test
+    public void testEchoedResponse() throws Exception {
+        String msg = "echo!";
+        final List<Object> expected = Collections.<Object>singletonList(msg);
+        final List<Object> received = new CopyOnWriteArrayList<Object>();
+        final CountDownLatch latch = new CountDownLatch(1);
+        
         ActorRef actorRef = harness.newActor(new ActorSpec(EchoActor.class.getName(), "Echo Actor"));
-        harness.sendTo(actorRef, "echo!");
-        // TODO Test requires manual inspection that the actor did its println
+        
+        harness.addMessageListener(new MessageListener() {
+                @Override public void onMessage(Object payload, MessageContext messageContext) {
+                    received.add(payload);
+                    latch.countDown();
+                }
+            });
+        
+        harness.sendTo(actorRef, msg);
+
+        latch.await(TIMEOUT, TimeUnit.MILLISECONDS);
+        Assert.assertEquals(received, expected);
     }
 
 }
